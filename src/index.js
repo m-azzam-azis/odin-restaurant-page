51a9c27277220cd9ba0c1f41bc60778409868bff
m.azzam <m.azzam.azis@gmail.com>
2024-04-27 16:57:44 +0700

    init the project until boilerplate html js css

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const menu = document.getElementById("menu");
+
+home.addEventListener("click", (e) => {
+  alert(e);
+});
+
+about.addEventListener("click", (e) => {});
+
+menu.addEventListener("click", (e) => {});
